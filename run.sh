138b68e8a7a1b845460033da8d5cb65e95b80507
gengjie02 <gengjie02@meituan.com>
2018-10-30 17:42:48 +0800

    modify build & deploy config

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-/webapps/myApp/beego-demo
+process=`ps aux|grep beego-demo|grep webapps|wc -l`
+
+if [ $process -eq 1 ]
+then
+        pid=`ps aux|grep beego-demo|grep webapps|awk -F " " '{print $2}'`
+        kill -9 $pid
+        echo "killed old process"
+else
+        echo "no old process"
+fi
+
+echo "start new process"
+nohup /webapps/myApp/beego-demo/beego-demo > /tmp/beego.log 2>&1 &
 
 
