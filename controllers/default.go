1e552ee4038913f8eb4c91507e37aa30eade6863
gengjie02 <gengjie02@meituan.com>
2019-09-26 16:11:15 +0800

    test ci

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
+	c.Data["Website"] = "beego.me"2
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
 }
